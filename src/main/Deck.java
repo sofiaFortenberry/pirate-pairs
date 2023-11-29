dd77a4caea2bcce593e724d8bf5917cb3af0497f
sofiaFortenberry <149592970+sofiaFortenberry@users.noreply.github.com>
2023-11-29 14:43:41 +0000

    wed2

--- src/main/Deck.java
+++ src/main/Deck.java
@@ -1,18 +1,21 @@
-public class Deck{
+import java.util.Arrays;
 
-    
+class Deck{
 
-    public static void main(String[] args){
-        private int amtCards = 55;
-        private int[] cards = new int[55];
+    final private int amtCards = 55;
+    private int[] cards = new int[amtCards];
+    private int highestOrderCard = 10;
+    private int count = 0;
 
-        for(int i = 0; i<amtCards; i++){
-            for(int k = i; k<amtCards; k++){
-                 cards[i] += i
-             }
-         }
-        System.out.println(cards);
+public String deckOfCards(){
+    for(int i = 0; i<=highestOrderCard; i++){
+        for(int k = 0; k<i; k++){
+            cards[count] = i;
+            count++;
+        }
     }
+    return(Arrays.toString(cards));
+}
 }
 
 
---
+++ src/main/PiratePairs.java
@@ -0,0 +1,6 @@
+public class PiratePairs {
+    public static void main(String[] args){
+        Deck newDeck = new Deck();
+        System.out.println(newDeck.deckOfCards());
+    }
+}
