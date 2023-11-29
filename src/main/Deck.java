b418c3b4cec9a68c83741a6ca874d1b3849b87f3
sofiaFortenberry <149592970+sofiaFortenberry@users.noreply.github.com>
2023-11-29 14:43:25 +0000

    wednesday

---
+++ src/main/Deck.java
@@ -0,0 +1,18 @@
+public class Deck{
+
+    
+
+    public static void main(String[] args){
+        private int amtCards = 55;
+        private int[] cards = new int[55];
+
+        for(int i = 0; i<amtCards; i++){
+            for(int k = i; k<amtCards; k++){
+                 cards[i] += i
+             }
+         }
+        System.out.println(cards);
+    }
+}
+
+
